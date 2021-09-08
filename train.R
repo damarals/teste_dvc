66f331fe39fc9e78e4aebb1e96703669df66f6b1
Daniel Amaral <silva.daniel86@Gmail.com>
2021-09-08 12:28:10 -0300

    teste 1

---
+++ train.R
@@ -0,0 +1,34 @@
+da_rec <- recipes::recipe(Species ~ Sepal.Length, data = iris)
+
+nnet_spec <- parsnip::mlp() |>
+  parsnip::set_engine("nnet") |>
+  parsnip::set_mode("classification")
+
+nnet_wflow <- workflows::workflow() |>
+  workflows::add_recipe(da_rec) |>
+  workflows::add_model(nnet_spec)
+
+nnet_fit <- nnet_wflow |>
+  parsnip::fit(iris)
+
+y_pred <- nnet_fit |>
+  predict(iris) |>
+  dplyr::pull()
+
+p <- ggplot2::qplot(x = Species, y = Sepal.Length,
+               data = iris, geom = 'boxplot')
+ggplot2::ggsave(filename = 'plot.png', plot = p, device = 'png',
+                  width = 3, height = 1.5, dpi = 300)
+
+cm <- iris |>
+  dplyr::transmute(truth = Species,
+                   estimate = y_pred)
+
+table(cm$truth, cm$estimate) |>
+  kableExtra::kable() |>
+  cat(file = 'cm.html')
+
+cm |>
+  yardstick::accuracy(truth = truth, estimate = estimate) |>
+  jsonlite::toJSON() |>
+  jsonlite::write_json(path = 'metrics.json')
